87444bdff862c1bcef6a08abdd25aeef16675e79
Athul Menon <52874407+Athul17x@users.noreply.github.com>
2019-11-08 10:46:42 +0530

    Create Circle.m

---
+++ Circle.m
@@ -0,0 +1,6 @@
+r = input(" enter the radius:");
+a = zeros(1,2);
+c=a;
+c(1,1)=input("Enter the x coordinate of the cenet with respect to origin:");
+c(1,2)=input("Enter the x coordinate of the cenet with respect to origin:");
+centroid = c
