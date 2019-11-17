970771f60886c20b141117b2ddea6ddb0a95b600
Caster12 <57310589+Caster12@users.noreply.github.com>
2019-11-17 12:07:21 +0530

    Update Rectangle_with_hole.m

--- Rectangle_with_hole.m
+++ Rectangle_with_hole.m
@@ -2,7 +2,8 @@ l=input("Enter l: ");
 b=input("Enter b: ");
 r=input("Enter the adius of the hole:");
 x=l+b;
-
+areaRect=l*b;
+areaCir= (pi*((r)^2))
 a=zeros(1,2);
 c1=a;
 c2=a;
@@ -29,20 +30,49 @@ c=a;
  b = d34+d14 ;
  flag =0;
  
- disp("Checking if inout is valid")
+ disp("Checking if input is valid")
+ 
+ d1 = sqrt((c1(1)-c(1))^2 + (c1(2)-c(2))^2);
+ d2 = sqrt((c2(1)-c(1))^2 + (c2(2)-c(2))^2);
+ d3 = sqrt((c3(1)-c(1))^2 + (c3(2)-c(2))^2);
+ d4 = sqrt((c4(1)-c(1))^2 + (c4(2)-c(2))^2);
  
  if(a==x && b==x)
      disp(" valid");
-     flag=1;
+     flg=1;
      if(
  else
      disp("invalid");
-     flag=2;
+     flg=2;
  end
- if(flag==1)
+ if(flg==1)
      m1=(c1+c2)/2 ;
      m2= (c3+c4)/2 ;
-     centroid = (m1+m2)/2 
+     centroidRect = (m1+m2)/2 
+     
  else
      disp("Calculation not possible")
  end
+ 
+ centroidCir = c
+ 
+if(flg=1)
+     if(d1>r && d2>r)
+      if(d3>r && d4>r)
+         disp("Valid");
+         flag = 1;
+      else
+         disp("invalid");
+      end
+     else
+     disp("invalid");
+     end
+end
+ 
+ 
+        
+        
+ if(flag==1)
+     centroid = ((centroidRect*areaRect)-(centroidCir*areaCir))/ (areaRect -areaCir)
+ else
+     disp("Calculation not possible");
